7f1faecbb4b3114dd4f0c2402335fb877f85d166
= <sasamotokazutoyo@gmail.com>
2023-10-18 22:49:51 +0900

    10/18/2235

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create!(
-  user_name: "test",
-  user_id: "test",
-)
